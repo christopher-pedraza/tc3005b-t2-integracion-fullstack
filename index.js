d5c0fea7c9012c6388be34fa13ada9dad22ac2de
Christopher Pedraza <gabalixreborn@gmail.com>
2024-05-02 12:24:46 -0600

    Add static file serving for dist folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-
+app.use(express.static("dist"));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
