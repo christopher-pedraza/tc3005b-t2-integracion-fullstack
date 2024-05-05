14dc69bd391e239764c20f6aad5a69822b987c80
Christopher Pedraza <gabalixreborn@gmail.com>
2024-05-05 15:59:10 -0600

    Agregar env

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
-const port = process.env.PORT || 3000;
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.static("dist"));
 
+//middleware
+app.use(express.json());
+app.use(cors());
+
+// Middleware de registro de solicitudes
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
+    next();
+};
+app.use(requestLogger);
+
+// Middleware de manejo de errores
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+};
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
